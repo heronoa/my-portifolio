8dc88ddfd7ba88c72a15b27d14e827aea774b0d9
heronoa <heron.amaral@gmail.com>
2024-11-29 01:47:20 -0300

    feat: creating notification if the user try to see projects in grid mode on small screens

--- src/components/UI/DisplayProjects/index.tsx
+++ src/components/UI/DisplayProjects/index.tsx
@@ -1,18 +1,30 @@
 "use client";
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { Button, ToggleButtonGroup, ToggleButton } from "@mui/material";
 import ViewListIcon from "@mui/icons-material/ViewList";
 import ViewModuleIcon from "@mui/icons-material/ViewModule";
 import DraggableGrid from "../Animations/DraggableGrid";
 import InteractableCarousel from "../Carousel/InteractableCarousel";
+import { AnimatePresence, motion } from "framer-motion";
 
 const DisplayProjects: React.FC = () => {
   const [view, setView] = useState<"carousel" | "grid">("carousel");
+  const [notificationOn, setNotificationOn] = useState(false);
+  let notificationTimeout: NodeJS.Timeout;
 
   const handleChange = (
     event: React.MouseEvent<HTMLElement>,
     nextView: "carousel" | "grid",
   ) => {
+    if (window.innerWidth < 1024) {
+      setNotificationOn(true);
+
+      notificationTimeout = setTimeout(() => {
+        setNotificationOn(false);
+      }, 5000);
+      return;
+    }
+
     setView(nextView);
   };
 
@@ -87,33 +99,51 @@ const DisplayProjects: React.FC = () => {
   ];
 
   return (
-    <div className="container mx-auto p-4 items-start">
-      <ToggleButtonGroup
-        className="hidden lg:inline-flex"
-        orientation="horizontal"
-        value={view}
-        exclusive
-        onChange={handleChange}
-      >
-        <ToggleButton value="carousel" aria-label="carousel">
-          <ViewListIcon />
-        </ToggleButton>
-        <ToggleButton value="grid" aria-label="grid">
-          <ViewModuleIcon />
-        </ToggleButton>
-      </ToggleButtonGroup>
-      <div className="mt-4 flex justify-center items-start min-h-[1000px] sm:min-h-[900px]">
-        {view === "carousel" ? (
-          <InteractableCarousel
-            className="max-w-screen sm:max-w-[500px] lg:max-w-[100vw] "
-            slides={projects}
-            options={{ loop: true }}
-          />
-        ) : (
-          <DraggableGrid items={projects} />
-        )}
+    <>
+      <div className="container mx-auto p-4 items-start">
+        <ToggleButtonGroup
+          className=""
+          orientation="horizontal"
+          value={view}
+          exclusive
+          onChange={handleChange}
+        >
+          <ToggleButton value="carousel" aria-label="carousel">
+            <ViewListIcon />
+          </ToggleButton>
+          <ToggleButton value="grid" aria-label="grid">
+            <ViewModuleIcon />
+          </ToggleButton>
+        </ToggleButtonGroup>
+        <div className="mt-4 flex justify-center items-start min-h-[1000px] sm:min-h-[900px]">
+          {view === "carousel" ? (
+            <InteractableCarousel
+              className="max-w-screen sm:max-w-[500px] lg:max-w-[100vw] "
+              slides={projects}
+              options={{ loop: true }}
+            />
+          ) : (
+            <DraggableGrid items={projects} />
+          )}
+        </div>
       </div>
-    </div>
+      <AnimatePresence>
+        {notificationOn && (
+          <motion.div
+            id="notification"
+            className="absolute top-0 -right-10 bg-primary-light opacity-0 dark:bg-dark-light dark:text-white text-black p-2 rounded-lg"
+            animate={{ x: -200, opacity: 1 }}
+            exit={{ x: 200, opacity: 0 }}
+            onClick={() => {
+              clearTimeout(notificationTimeout);
+              setNotificationOn(false);
+            }}
+          >
+            Check that feature on desktop please
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </>
   );
 };
 
